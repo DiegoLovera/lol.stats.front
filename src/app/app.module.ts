72532f86bd514beb6a0cd0358663ffc02cca47f7
Diego Lovera <diegolovera010@gmail.com>
2020-03-05 09:05:55 -0500

    componentes agregados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchComponent } from './search/search.component';
+import { MatchesComponent } from './matches/matches.component';
+import { StatsComponent } from './stats/stats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    MatchesComponent,
+    StatsComponent
   ],
   imports: [
     BrowserModule,

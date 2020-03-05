3b213770f693efc5e457feb8cf788810c10bd7e6
Diego Lovera <diegolovera010@gmail.com>
2020-03-05 13:05:46 -0500

    buscador agregado con filtro por queues

--- src/app/matches/matches.component.ts
+++ src/app/matches/matches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-matches',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./matches.component.css']
 })
 export class MatchesComponent implements OnInit {
+  summonerName = '';
+  soloq = true;
+  flex = true;
 
-  constructor() { }
+  constructor(private _Activatedroute: ActivatedRoute,
+    private _router: Router) { }
+
+  sub;
 
   ngOnInit(): void {
+    this.sub = this._Activatedroute.paramMap.subscribe(params => {
+      console.log(params);
+      this.summonerName = params.get('summonerName');
+    });
+    this._Activatedroute.queryParamMap
+      .subscribe(params => {
+
+        this.soloq = JSON.parse(params.get('soloq'));
+        this.flex = JSON.parse(params.get('flex'));
+      });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  onBack(): void {
+    this._router.navigate(['']);
   }
 
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  summonerName = '';
+  soloq = true;
+  flex = true;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSummonerSet(event: any) {
+    this.summonerName = event.target.value;
+  }
+
+  soloqChange(event:MatCheckboxChange): void {
+    this.soloq = event.checked;
+  }
+
+  flexChange(event:MatCheckboxChange): void {
+    this.flex = event.checked;
+  }
+
+  onClickSearch() {
+    this.router.navigate(['/Summoner', this.summonerName], { queryParams: { soloq: this.soloq, flex: this.flex } });
   }
 
 }
